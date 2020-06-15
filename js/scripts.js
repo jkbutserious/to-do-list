ad9b919887ab3732d51c552dfdb02607c880c0e8
Jeff Kim <kim2jy@hotmail.com>
2020-06-15 15:27:01 -0700

    Refactor task list display, add functions to allow users to mark tasks complete, add functions to remove completed tasks
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,26 @@ ToDoList.prototype.assignId = function() {
   return this.currentID;
 }
 
+ToDoList.prototype.markComplete = function (taskId) {
+  for (let i = 0; i < this.tasks.length; i++) {
+    if (this.tasks[i]){
+      if(this.tasks[i].id === taskId) {
+        this.tasks[i].complete = true;
+      }
+    }
+  }
+}
+
+ToDoList.prototype.removeComplete = function () {
+  for (let i = 0; i < this.tasks.length; i++) {
+    if (this.tasks[i]){
+      if (this.tasks[i].complete === true) {
+        delete this.tasks[i];
+      }
+    } 
+  }
+}
+
 function Task(taskName, complete) {
   this.taskName = taskName;
   this.complete = complete;
@@ -22,15 +42,39 @@ function Task(taskName, complete) {
 
 let list1 = new ToDoList();
 
+
+
 // User Interface Logic
 
 $(document).ready(function() {
-  $("#toDoList").submit(function() {
+  $("#toDoList").submit(function(event) {
     event.preventDefault();
     const task = $("#task").val();
     let taskItem = new Task(task, false);
     list1.addTask(taskItem);
+    $("#output").empty();
 
-    
-  })
-})
+    for (let i = 0; i < list1.tasks.length; i++) {
+      // let test = list1.tasks[i].taskName;
+      if(list1.tasks[i]) {
+        $("#output").append("<li>" + list1.tasks[i].taskName + " ID: " + list1.tasks[i].id + "</li>");
+      }
+    }
+  });
+  $("#completeTask").submit(function(event) {
+    event.preventDefault();
+    list1.markComplete(parseInt($("#completed-task").val()));
+  });
+  $("#removeTasks").click(function() {
+    list1.removeComplete();
+    $("#output").empty();
+
+    for (let i = 0; i < list1.tasks.length; i++) {
+      // let test = list1.tasks[i].taskName;
+      if(list1.tasks[i]) {
+        $("#output").append("<li>" + list1.tasks[i].taskName + " ID: " + list1.tasks[i].id + "</li>");
+      }
+    }
+  });
+
+});
